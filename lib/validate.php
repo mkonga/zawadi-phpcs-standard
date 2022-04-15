697597118d0ec6835846f8889e2358593c94dcab
Daan van den Berg <git@doppy.nl>
2022-04-15 11:53:11 +0200

    fix return error codes

--- lib/validate.php
+++ lib/validate.php
@@ -8,16 +8,18 @@ $xsdFile = $argv[2];
 
 foreach ([$xmlFile, $xsdFile] as $file) {
     if (!file_exists($file)) {
-        die(sprintf(
+        echo sprintf(
             'File "%s" is not found.' . PHP_EOL,
             $file
-        ));
+        );
+        die(1);
     }
     if (!is_readable($file)) {
-        die(sprintf(
+        echo sprintf(
             'File "%s" is not readable.' . PHP_EOL,
             $file
-        ));
+        );
+        die(2);
     }
 }
 
@@ -25,10 +27,11 @@ $dom = new DOMDocument();
 $dom->load($xmlFile);
 
 if (!$dom->schemaValidate($xsdFile)) {
-    die(sprintf(
+    echo sprintf(
         'File "%s" is not valid according to xsd.' . PHP_EOL,
         $file
-    ));
+    );
+    die(3);
 }
 
 echo sprintf('File "%s" is ok.', $xmlFile) . PHP_EOL;
